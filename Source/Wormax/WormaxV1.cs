a50e7ed7cab36358def8cf04d61f0191a304f573
Noah Johnson <noahmjohnson1110@gmail.com>
2024-11-01 21:33:37 +0000

    Add project files.

---
+++ Source/Wormax/WormaxV1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Wormax
+{
+    public class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
+    {
+        public HediffDef removeThisThing;
+
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested, int ingestedCount)
+        {
+            var gutwormsHediff = pawn.health.hediffSet.GetFirstHediffOfDef(removeThisThing);
+            if (gutwormsHediff != null)
+            {
+                pawn.health.hediffSet.hediffs.Remove(gutwormsHediff);
+            }
+        }
+    }
+}
